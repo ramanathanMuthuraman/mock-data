4c72482306aca8a738c1ad17fa5f569ecb6d07fa
Ramanathan M <ramanathanm@Ramanathans-MacBook-Pro.local>
2021-04-04 14:42:47 +0530

    add cors support

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const faker = require("faker");
+const cors = require("cors");
 const _ = require("lodash");
 
 const app = express();
 
-app.post("/auth", (req, res) => {
-  res.send({
-    token:
-      "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYxNzQzNzM5NywiaWF0IjoxNjE3MzUwOTk3fQ.s2Pb6XsfMTCoa-q7g1Py3vm9K2vVyVb2SuBnIctGvv5XqoThhDOvO8YTnU1IS4F-qN4rI4BwcaVdIG9wW9JmMA",
-    tokenExpiryTime: 86400,
-    user: {
-      userName: "admin",
-      email: "admin@gmail.com",
-      empId: "-1",
-      roleName: "SUPER_ADMIN",
-      phoneNumber: null,
-      modules: ["FM", "UM", "AM", "CRA"],
-    },
-  });
-});
+app.use(cors());
 
-app.get("/auth", (req, res) => {
+app.post("/auth", (req, res) => {
   res.send({
     token:
       "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYxNzQzNzM5NywiaWF0IjoxNjE3MzUwOTk3fQ.s2Pb6XsfMTCoa-q7g1Py3vm9K2vVyVb2SuBnIctGvv5XqoThhDOvO8YTnU1IS4F-qN4rI4BwcaVdIG9wW9JmMA",
